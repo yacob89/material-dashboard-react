666a02035b15950599751de454ac965de7bad6dc
Yacob Madiana <yacob.madiana@gmail.com>
2018-08-15 15:55:34 +0700

    Change color done

--- src/views/UploadList/UploadList.jsx
+++ src/views/UploadList/UploadList.jsx
@@ -11,7 +11,30 @@ import filterFactory, { textFilter } from 'react-bootstrap-table2-filter';
 
 // Utils
 import auth from 'utils/auth';
-import request from 'utils/request';
+
+import Loader from 'react-loader-advanced';
+import Spinner from 'react-spinkit';
+import { SwatchesPicker } from 'react-color';
+import Modal from 'react-modal';
+
+const customStyles = {
+  content : {
+    top                   : '50%',
+    left                  : '50%',
+    right                 : 'auto',
+    bottom                : 'auto',
+    marginRight           : '-50%',
+    transform             : 'translate(-50%, -50%)'
+  }
+};
+
+const customMessageElement = (
+  <div>custom message element</div>
+);
+
+const spinner = (
+  <Spinner name="line-scale"/>
+);
 
 const SERVER_URL = 'http://54.245.202.137';
 //const SERVER_URL = 'http://192.168.1.13';
@@ -145,6 +168,39 @@ const columns = [{
     events: {
       onClick: () => console.log('Edit on this layer')
     }
+  }, {
+    dataField: 'color',
+    text: 'Color',
+    headerStyle: {
+      backgroundColor: '#6495ED'
+    },
+    events: {
+      onClick: () => alert('Delete layer')
+    }
+  }
+  , {
+    dataField: 'icon',
+    text: 'Icon',
+    filter: textFilter(),
+    sort: true,
+    headerStyle: {
+      backgroundColor: '#6495ED'
+    },
+    style: (cell, row, rowIndex, colIndex) => {
+      return {
+        backgroundColor: 'white'
+      };
+    },
+    editor: {
+      type: Type.SELECT,
+      options: [{
+        value: 'museum-15',
+        label: 'museum-15'
+      }, {
+        value: 'music-11',
+        label: 'music-11'
+      }]
+    }
   }
 ];
 
@@ -153,6 +209,8 @@ const rowEvents = {
     alert(`Layer Deleted at: ${rowIndex}`);
   }
 };
+
+let rows = [];
 
 class UploadList extends React.Component {
   constructor(props) {
@@ -163,12 +221,35 @@ class UploadList extends React.Component {
       rowData: [],
       rowID: 0,
       newValue: '',
-      totalsize: 0
+      totalsize: 0,
+      showModal: false,
+      displayColorPicker: false,
+      background: '#fff',
+      color: {
+        r: '241',
+        g: '112',
+        b: '19',
+        a: '1',
+      },
+      colorPicker: false,
+      selectedID:'',
+      selectedLocation:'',
+      selectedColor:'',
+      selectedIcon:'',
+      selectedFilename:''
     }
     this.loadFileList = this.loadFileList.bind(this);
     this.updateRemoteData = this.updateRemoteData.bind(this);
     this.updateGeojsonType = this.updateGeojsonType.bind(this);
     this.updateFilename = this.updateFilename.bind(this);
+    this.updateGeojsonColor = this.updateGeojsonColor.bind(this);
+    this.updateGeojsonIcon = this.updateGeojsonIcon.bind(this);
+    this.testGeojsonUpdate = this.testGeojsonUpdate.bind(this);
+
+    this.openModal = this.openModal.bind(this);
+    this.afterOpenModal = this.afterOpenModal.bind(this);
+    this.closeModal = this.closeModal.bind(this);
+    this.handleChange = this.handleChange.bind(this);
   }
 
   componentDidMount() {
@@ -191,7 +272,7 @@ class UploadList extends React.Component {
 
   loadFileList() {
     // Make a request for a user with a given ID
-    let rows = [];
+    rows = [];
 
       axios.get(SERVER_URL+':1337/fileuploads', {
           params: {
@@ -214,12 +295,14 @@ class UploadList extends React.Component {
             active: fileList[i].active,
             filesize: fileList[i].filesize/1000000,
             _id: fileList[i]._id,
+            color: fileList[i].color,
+            icon: fileList[i].icon,
             delete:"Delete",
             edit:"Edit"
           });
           total = total + fileList[i].filesize;
         }
-        this.setState({ rowData:rows, totalsize: total });
+        this.setState({ rowData:rows, totalsize: total, showModal: false });
       });
   }
 
@@ -252,6 +335,9 @@ class UploadList extends React.Component {
   updateRemoteData(id, activevalue) {
     console.log("Row ID: ", id);
     console.log("Row Active Value: ", activevalue);
+    this.setState({
+      showModal: true
+    });
 
     var promise = new Promise(function (resolve, reject) {
       axios.put(`http://54.245.202.137:1337/fileuploads/${id}`, /*{
@@ -282,6 +368,9 @@ class UploadList extends React.Component {
   }
 
   updateGeojsonType(id, typevalue) {
+    this.setState({
+      showModal: true
+    });
     console.log("Row ID: ", id);
     console.log("Row Type Value: ", typevalue);
 
@@ -314,6 +403,9 @@ class UploadList extends React.Component {
   }
 
   updateFilename(id, namevalue) {
+    this.setState({
+      showModal: true
+    });
     console.log("Row ID: ", id);
     console.log("Row Type Value: ", namevalue);
 
@@ -345,68 +437,250 @@ class UploadList extends React.Component {
     promise.then(bool => this.loadFileList())
   }
 
+  updateGeojsonColor(id, color) {
+    this.setState({
+      showModal: true
+    });
+    console.log("Row ID: ", id);
+    console.log("Row Type Value: ", color);
+
+    var promise = new Promise(function (resolve, reject) {
+      axios.put(`http://54.245.202.137:1337/fileuploads/${id}`, /*{
+      //axios.put(`http://192.168.1.2:1337/fileuploads/${id}`,
+          /*{
+                 params: {
+                   _id:id
+                 }
+               },*/
+          {
+            color: color
+          })
+        .then(function (responses) {
+          console.log("Respon Data: ", responses.data);
+          if (responses.data.ok == 1) {
+            alert('Success!');
+          }
+          resolve('true');
+        })
+        .catch(function (error) {
+          console.log(error);
+          alert(error);
+        });
+      // call resolve if the method succeeds
+      
+    })
+    promise.then(bool => this.loadFileList())
+    
+  }
+
+  updateGeojsonIcon(id, icon) {
+    this.setState({
+      showModal: true
+    });
+    console.log("Row ID: ", id);
+    console.log("Row Type Value: ", icon);
+
+    var promise = new Promise(function (resolve, reject) {
+      axios.put(`http://54.245.202.137:1337/fileuploads/${id}`, /*{
+      //axios.put(`http://192.168.1.2:1337/fileuploads/${id}`,
+          /*{
+                 params: {
+                   _id:id
+                 }
+               },*/
+          {
+            icon: icon
+          })
+        .then(function (responses) {
+          console.log("Respon Data: ", responses.data);
+          if (responses.data.ok == 1) {
+            alert('Success!');
+          }
+          resolve('true');
+        })
+        .catch(function (error) {
+          console.log(error);
+          alert(error);
+        });
+      // call resolve if the method succeeds
+      
+    })
+    promise.then(bool => this.loadFileList())
+    
+  }
+
+  testGeojsonUpdate(id, location, color, icon, filename){
+    this.setState({
+      showModal: true
+    });
+    var promise = new Promise(function (resolve, reject) {
+      axios.post('http://192.168.1.14' + ':7555/updatelayer', {
+        location: location,
+        color: color,
+        icon: icon,
+        id: id,
+        username: auth.getUserInfo().username,
+        filename: filename
+      })
+      .then(function (response) {
+        console.log(response.data);
+        if (response.data == 'success') {
+          console.log("Server side converting success");
+        }
+        resolve('true');
+      })
+      .catch(function (error) {
+        console.log("Server side converting error: ",error)
+      });
+    })
+    promise.then(bool => this.loadFileList())
+  }
+
+  handleChange(color, event) {
+    // color = {
+    //   hex: '#333',
+    //   rgb: {
+    //     r: 51,
+    //     g: 51,
+    //     b: 51,
+    //     a: 1,
+    //   },
+    //   hsl: {
+    //     h: 0,
+    //     s: 0,
+    //     l: .20,
+    //     a: 1,
+    //   },
+    // }
+    console.log('Color: ', color.hex);
+    this.setState({background: color.hex,
+    selectedColor:color.hex});
+  }
+
+  openModal(id, location, color, icon, filename) {
+    this.setState({colorPicker: true,
+      selectedID:id,
+      selectedLocation:location,
+      selectedColor:color,
+      selectedIcon:icon,
+      selectedFilename:filename});
+  }
+
+  afterOpenModal() {
+    // references are now sync'd and can be accessed.
+    this.subtitle.style.color = '#f00';
+  }
+
+  closeModal() {
+    this.setState({colorPicker: false});
+    this.testGeojsonUpdate(this.state.selectedID, this.state.selectedLocation, this.state.selectedColor, this.state.selectedIcon, this.state.selectedFilename);
+  }
+
   render() {
+
+    const rowStyle2 = (row, rowIndex) => {
+      const style = {};
+      style.backgroundColor = rows[rowIndex].color;
+    
+      return style;
+    };
+
     return (
-      <Grid container>
-        <ItemGrid xs={12} sm={12} md={12}>
-          <RegularCard
-            headerColor="blue"
-            plainCard
-            cardTitle="Current Active Layers"
-            cardSubtitle={'Total Upload Size: '+ this.state.totalsize / 1000000+ ' MB'}
-            content={
-              <BootstrapTable
-                keyField="id"
-                data={this.state.rowData}
-                columns={columns}
-                striped
-                hover
-                condensed
-                noDataIndication="No Layer is Uploaded"
-                filter={filterFactory()}
-                cellEdit={cellEditFactory({
-                  mode: 'click',
-                  blurToSave: true,
-                  beforeSaveCell: (oldValue, newValue, row, column) => { console.log('Before Saving Cell!!'); },
-                  afterSaveCell: (oldValue, newValue, row, column) => {
-                    console.log('After Saving Cell!!', ' row ', row._id);
-                    console.log('After saving cell column: ', column);
-                    if (column.dataField == "active") {
-                      this.setState({
-                        rowID: row._id,
-                        newValue: newValue
-                      });
-                      this.updateRemoteData(row._id, newValue);
-                    }
-                    if (column.dataField == "type") {
-                      this.setState({
-                        rowID: row._id,
-                        newValue: newValue
-                      });
-                      this.updateGeojsonType(row._id, newValue);
-                    }
-                    if (column.dataField == "filename") {
-                      this.setState({
-                        rowID: row._id,
-                        newValue: newValue
-                      });
-                      this.updateFilename(row._id, newValue);
-                    }
-                    if(column.dataField == "delete"){
-                      console.log("Delete activated!");
-                      this.deleteRemoteData(row._id);
-                    }
-                    if(column.dataField == "edit"){
-                      console.log("Edit Layer activated!");
-                      this.editRemoteData(row.location);
-                    }
-                  }
-                })}
+      <div>
+        <Loader show={this.state.showModal} message={spinner}>
+        <Modal
+          isOpen={this.state.colorPicker}
+          onAfterOpen={this.afterOpenModal}
+          onRequestClose={this.closeModal}
+          style={customStyles}
+          contentLabel="Example Modal"
+        >
+
+          <h3 ref={subtitle => this.subtitle = subtitle}>Select layer color</h3>
+          <button onClick={this.closeModal}>close</button>
+          <SwatchesPicker onChange={ this.handleChange } />
+        </Modal>
+          <Grid container>
+            <ItemGrid xs={12} sm={12} md={12}>
+              <RegularCard
+                headerColor="blue"
+                plainCard
+                cardTitle="Current Active Layers"
+                cardSubtitle={'Total Upload Size: ' + this.state.totalsize / 1000000 + ' MB'}
+                content={
+                  <BootstrapTable
+                    keyField="id"
+                    data={this.state.rowData}
+                    columns={columns}
+                    striped
+                    hover
+                    condensed
+                    rowStyle={ rowStyle2 }
+                    noDataIndication="No Layer is Uploaded"
+                    filter={filterFactory()}
+                    cellEdit={cellEditFactory({
+                      mode: 'click',
+                      blurToSave: true,
+                      beforeSaveCell: (oldValue, newValue, row, column) => { console.log('Before Saving Cell!!'); },
+                      afterSaveCell: (oldValue, newValue, row, column) => {
+                        console.log('After Saving Cell!!', ' row ', row._id);
+                        console.log('After saving cell column: ', column);
+                        if (column.dataField == "active") {
+                          this.setState({
+                            rowID: row._id,
+                            newValue: newValue
+                          });
+                          this.updateRemoteData(row._id, newValue);
+                        }
+                        if (column.dataField == "type") {
+                          this.setState({
+                            rowID: row._id,
+                            newValue: newValue
+                          });
+                          this.updateGeojsonType(row._id, newValue);
+                        }
+                        if (column.dataField == "filename") {
+                          this.setState({
+                            rowID: row._id,
+                            newValue: newValue
+                          });
+                          this.updateFilename(row._id, newValue);
+                        }
+                        if (column.dataField == "delete") {
+                          console.log("Delete activated!");
+                          this.deleteRemoteData(row._id);
+                          //this.testGeojsonUpdate(row._id, row.location, row.color, row.icon, row.filename, row._id)
+                        }
+                        if (column.dataField == "edit") {
+                          console.log("Edit Layer activated!");
+                          this.editRemoteData(row.location);
+                        }
+                        if (column.dataField == "color") {
+                          this.setState({
+                            rowID: row._id,
+                            newValue: newValue
+                          });
+                          //this.updateGeojsonColor(row._id, newValue);
+                          //this.testGeojsonUpdate(row._id, row.location, newValue, row.icon, row.filename, row._id)
+                          this.openModal(row._id, row.location, newValue, row.icon, row.filename, row._id);
+                        }
+                        if (column.dataField == "icon") {
+                          this.setState({
+                            rowID: row._id,
+                            newValue: newValue
+                          });
+                          //this.updateGeojsonIcon(row._id, newValue);
+                          this.testGeojsonUpdate(row._id, row.location, row.color, newValue, row.filename, row._id)
+                        }
+                      }
+                    })}
+                  />
+                }
               />
-            }
-          />
-        </ItemGrid>
-      </Grid>
+            </ItemGrid>
+          </Grid>
+        </Loader>
+      </div>
     );
   }
 }
